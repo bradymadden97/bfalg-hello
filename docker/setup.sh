17cb591e070cc640da40d0c867fa54df6dadcaf0
wkhunt <wkhunt3@gmail.com>
2017-07-10 10:53:09 -0400

    Update setup.sh

--- docker/setup.sh
+++ docker/setup.sh
@@ -1,1 +1,7 @@
-echo This is the setup
+mkdir vendor
+
+pip install -d vendor numpy
+pip install -d vendor eyeD3
+pip install -d vendor Pillow
+pip install -d vendor pyproj
+pip install -d vendor gippy
