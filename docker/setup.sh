6e5e15912abc6c4c99017be255ebee14367714d9
wkhunt <wkhunt3@gmail.com>
2017-07-10 10:59:39 -0400

    Update setup.sh

--- docker/setup.sh
+++ docker/setup.sh
@@ -1,5 +1,9 @@
 mkdir vendor
 
+yum --enablerepo=extras install epel-release
+yum -y install git libgdal-dev python-setuptools g++ python-dev python-pip python-gdal gdal-bin
+    
+    
 pip install -d vendor numpy
 pip install -d vendor eyeD3
 pip install -d vendor Pillow
