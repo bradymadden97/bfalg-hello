6af7efa8cd77ff10bc2b1a8fa887e72df9c65159
ubuntu <wkhunt3@gmail.com>
2017-07-14 15:12:16 -0400

    Doing the build

--- docker/setup.sh
+++ docker/setup.sh
@@ -2,24 +2,5 @@ mkdir vendor
     
 export PATH="$HOME/miniconda2/bin:$PATH"
 
-#pwd
-#cd /
-#find | grep environment.yml
-
 cd built-repo
-echo What the container got
 ls
-mkdir -p conda-recipes/vendor
-cd conda-recipes/vendor
-git clone https://github.com/gipit/gippy
-cd ..
-pwd
-ls
-echo Whats in vendor
-ls vendor
-conda build gippy
-
-echo
-pwd
-ls
-python --version
