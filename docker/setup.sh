397125b95ffa17d7c69d00a80663555dc8104a53
wkhunt <wkhunt3@gmail.com>
2017-07-14 15:41:38 -0400

    Update setup.sh

--- docker/setup.sh
+++ docker/setup.sh
@@ -3,10 +3,9 @@ export PATH="$HOME/miniconda2/bin:$PATH"
 echo "#############################"
 pwd
 ls
-cd built-repo
-echo Whats in built-repo
-conda update -n root conda-build -y
-conda update --all -y
+cd conda-recipes
+#conda update -n root conda-build -y
+#conda update --all -y
 conda build agg
 conda config --add channels local
 conda build pypotrace
