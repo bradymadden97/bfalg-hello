f55161616fd847f3ac0d5887a13637c11a79e39a
wkhunt <wkhunt3@gmail.com>
2017-07-14 16:01:07 -0400

    Update setup.sh

--- docker/setup.sh
+++ docker/setup.sh
@@ -6,11 +6,11 @@ ls
 cd conda-recipes
 pwd
 ls
-#conda update -n root conda-build -y
-#conda update --all -y
-#conda build agg
-#conda config --add channels local
-#conda build pypotrace
-#conda build gippy
-#echo
-#echo ~/miniconda2/conda-bld
+conda update -n root conda-build -y
+conda update --all -y
+conda build agg
+conda config --add channels local
+conda build pypotrace
+conda build gippy
+echo
+echo ~/miniconda2/conda-bld
