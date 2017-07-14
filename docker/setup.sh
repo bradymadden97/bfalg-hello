317a34a402c475d5195558a3e88b49ccb78f0676
wkhunt <wkhunt3@gmail.com>
2017-07-14 16:07:53 -0400

    Update setup.sh

--- docker/setup.sh
+++ docker/setup.sh
@@ -7,7 +7,7 @@ cd conda-recipes
 pwd
 ls
 conda update -n root conda-build -y
-conda update --all -y
+#conda update --all -y
 conda build agg
 conda config --add channels local
 conda build pypotrace
