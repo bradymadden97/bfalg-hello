d92c5bf3cd2c9f4a4ac503a653d526ce456faf9a
wkhunt <wkhunt3@gmail.com>
2017-07-14 12:42:30 -0400

    Update setup.sh

--- docker/setup.sh
+++ docker/setup.sh
@@ -1,14 +1,8 @@
 mkdir vendor
+    
+export PATH="$HOME/miniconda2/bin:$PATH"
 
-yum --enablerepo=extras install epel-release
-yum -y install git libgdal-dev python-setuptools g++ python-dev python-pip python-gdal gdal-bin
-    
-    
-pip install -d vendor numpy
-pip install -d vendor eyeD3
-pip install -d vendor Pillow
-pip install -d vendor pyproj
-pip install -d vendor gippy
+conda env create -f environment.yml
 
 pwd
 ls
