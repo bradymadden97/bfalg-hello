25d334b38edc245f9e81bb01e18db79e4a023906
ubuntu <wkhunt3@gmail.com>
2017-07-18 13:04:46 -0400

    Added setup script

--- setup.sh
+++ setup.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 wd=`pwd`
-git clone https://github.com/venicegeo/wh-sandbox
 conda config --add channels conda-forge
 conda config --add channels bioconda
-conda config --add channels file://$wd/wh-sandbox
+conda config --add channels file://$wd/channel
 conda install numpy -y
 conda install potrace -y
 conda install pypotrace -y
@@ -13,5 +12,5 @@ conda install libagg=2.5.0 -y
 conda install fiona=1.7.4 -y
 conda install gippy -y
 pip install eyed3
-rm -rf wh-sandbox
+rm -rf channel
 python shape/shape.py -v
