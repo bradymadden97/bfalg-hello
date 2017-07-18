956e79d4c11c349e55713f6fa27a3fe192fe88a3
ubuntu <wkhunt3@gmail.com>
2017-07-18 10:12:21 -0400

    fixed script

--- setup.sh
+++ setup.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 wd=`pwd`
 git clone https://github.com/venicegeo/wh-sandbox
-conda create -n bfalg-shape -y
-source activate bfalg-shape
 conda config --add channels conda-forge
 conda config --add channels bioconda
 conda config --add channels file://$wd/wh-sandbox
@@ -15,4 +13,5 @@ conda install libagg=2.5.0 -y
 conda install fiona=1.7.4 -y
 conda install gippy -y
 pip install eyed3
+rm -rf wh-sandbox
 python shape/shape.py -v
