8f7d19478c071d0567f510bd1b39a38eb0c83e32
wkhunt <wkhunt3@gmail.com>
2017-07-18 09:49:16 -0400

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+wd=`pwd`
+git clone https://github.com/venicegeo/wh-sandbox
+conda create -n bfalg-shape -y
+source activate bfalg-shape
+conda config --add channels conda-forge
+conda config --add channels bioconda
+conda config --add channels file://$wd/wh-sandbox
+conda install numpy -y
+conda install potrace -y
+conda install pypotrace -y
+conda install pillow -y
+conda install pyproj -y
+conda install libagg=2.5.0 -y
+conda install fiona=1.7.4 -y
+conda install gippy -y
+pip install eyed3
+python shape/shape.py -v
