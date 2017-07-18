c4355bb6e1cacd1e26e2d4e25520fe5dec1012fc
ubuntu <wkhunt3@gmail.com>
2017-07-18 13:56:15 -0400

    test

--- setup.sh
+++ setup.sh
@@ -2,14 +2,14 @@ wd=`pwd`
 conda config --add channels conda-forge
 conda config --add channels bioconda
 conda config --add channels file://$wd/channel
-conda install numpy -y
-conda install potrace -y
-conda install pypotrace -y
-conda install pillow -y
-conda install pyproj -y
-conda install libagg=2.5.0 -y
-conda install fiona=1.7.4 -y
-conda install gippy -y
-pip install eyed3
+conda install numpy -y > /dev/null
+conda install potrace -y > /dev/null
+conda install pypotrace -y > /dev/null
+conda install pillow -y > /dev/null
+conda install pyproj -y > /dev/null
+conda install libagg=2.5.0 -y > /dev/null
+conda install fiona=1.7.4 -y > /dev/null
+conda install gippy -y > /dev/null
+pip install eyed3 > /dev/null
 rm -rf channel
 python shape/shape.py -v
