34f85701f0beccddf6c40ae7c2ec3fbaecce59ca
ubuntu <wkhunt3@gmail.com>
2017-08-15 14:43:45 -0400

    Testing pkg copy

--- docker/script.sh
+++ docker/script.sh
@@ -4,5 +4,6 @@ export PATH="$HOME/miniconda2/bin:$PATH"
 
 cd share
 conda env create -f environment.yml
-source activate bfalg-shape
-conda list
+rm environment.yml
+cp ~/miniconda2/pkgs/*.tar.bz2 .
+ls
