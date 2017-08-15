48090b03a454c23fde2bef3e69999bb3e9ca15e0
ubuntu <wkhunt3@gmail.com>
2017-08-15 15:25:08 -0400

    fix

--- docker/script.sh
+++ docker/script.sh
@@ -5,7 +5,8 @@ export PATH="$HOME/miniconda2/bin:$PATH"
 cd share
 conda env create -f environment.yml
 rm environment.yml
-cd ~/miniconda2/pkgs
-cp -a !(*.tar.bz2) /root/share
-cd root/share
+cp -a ~/miniconda2/pkgs/* .
+rm *.tar.bz2
+rm -rf cache
+rm urls && rm urls.txt
 ls
