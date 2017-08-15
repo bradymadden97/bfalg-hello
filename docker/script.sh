fa6312b55f10579b285cf351d80395d176b15e55
ubuntu <wkhunt3@gmail.com>
2017-08-15 15:14:58 -0400

    Copying the folders instead of the tars

--- docker/script.sh
+++ docker/script.sh
@@ -5,5 +5,7 @@ export PATH="$HOME/miniconda2/bin:$PATH"
 cd share
 conda env create -f environment.yml
 rm environment.yml
-cp ~/miniconda2/pkgs/*.tar.bz2 .
+cd ~/miniconda2/pkgs
+cp -a !(*.tar.bz2) /root/share
+cd root/share
 ls
