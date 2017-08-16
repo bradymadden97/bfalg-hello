f08b6fc28bf9f7ccfdfac551ab62f74b5c21a0c9
ubuntu <wkhunt3@gmail.com>
2017-08-16 10:52:44 -0400

    test

--- docker/script.sh
+++ docker/script.sh
@@ -2,10 +2,17 @@
 
 export PATH="$HOME/miniconda2/bin:$PATH"
 
-cd share
+#cd share
+#conda env create -f environment.yml
+#rm environment.yml
+#cp -a ~/miniconda2/pkgs/* .
+#rm *.tar.bz2
+#rm -rf cache
+#rm urls && rm urls.txt
+
+mkdir bfalg-shape
+mv share/* bfalg-shape
+cd bfalg-shape
 conda env create -f environment.yml
-rm environment.yml
-cp -a ~/miniconda2/pkgs/* .
-rm *.tar.bz2
-rm -rf cache
-rm urls && rm urls.txt
+source activate bfalg-shape
+python bfalg_shape/shape.py --version
